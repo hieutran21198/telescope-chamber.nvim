d9952faaed769eede479e81cacffffe8abd89442
Hieu Tran <hieu@buuuk.com>
2023-01-24 22:41:34 +0700

    remove unused exported telescope keys

--- lua/telescope/_extensions/chamber.lua
+++ lua/telescope/_extensions/chamber.lua
@@ -8,9 +8,8 @@ local chamber = require "chamber"
 return telescope.register_extension {
 	setup = chamber.setup,
 	exports = {
+		region = chamber.pick_region,
 		profile = chamber.pick_profile,
 		service = chamber.pick_service,
-		load = chamber.load_from_file,
-		value = chamber.pick_variable,
 	},
 }
